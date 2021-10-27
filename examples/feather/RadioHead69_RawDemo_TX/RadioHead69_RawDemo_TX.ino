8d342d4604053e4e87b56f935fa70913509453e4
lady ada <limor@ladyada.net>
2021-10-26 22:55:44 -0400

    simplify pin options, add esp32s2

--- examples/feather/RadioHead69_RawDemo_TX/RadioHead69_RawDemo_TX.ino
+++ examples/feather/RadioHead69_RawDemo_TX/RadioHead69_RawDemo_TX.ino
@@ -21,35 +21,38 @@
   #define RFM69_INT     7
   #define RFM69_RST     4
   #define LED           13
-#endif
-
-#if defined(ADAFRUIT_FEATHER_M0) // Feather M0 w/Radio
+  
+#elif defined(ADAFRUIT_FEATHER_M0) // Feather M0 w/Radio
   #define RFM69_CS      8
   #define RFM69_INT     3
   #define RFM69_RST     4
   #define LED           13
-#endif
-
-#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+  
+#elif defined (__AVR_ATmega328P__)  // Feather 328P w/wing
   #define RFM69_INT     3  // 
   #define RFM69_CS      4  //
   #define RFM69_RST     2  // "A"
   #define LED           13
-#endif
 
-#if defined(ESP8266)    // ESP8266 feather w/wing
+#elif defined(ESP8266)    // ESP8266 feather w/wing
   #define RFM69_CS      2    // "E"
   #define RFM69_IRQ     15   // "B"
   #define RFM69_RST     16   // "D"
   #define LED           0
-#endif
 
-#if defined(ESP32)    // ESP32 feather w/wing
+#elif defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+  #define RFM69_INT     9  // "A"
+  #define RFM69_CS      10  // "B"
+  #define RFM69_RST     11  // "C"
+  #define LED           13
+
+#elif defined(ESP32)    // ESP32 feather w/wing
   #define RFM69_RST     13   // same as LED
   #define RFM69_CS      33   // "B"
   #define RFM69_INT     27   // "A"
   #define LED           13
 #endif
+
 
 /* Teensy 3.x w/wing
 #define RFM69_RST     9   // "A"
